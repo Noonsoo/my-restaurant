24c029c0db374d72312f0cfebc0b994b2fd1bbe2
Noonsoo <nwafornonso70@gmail.com>
2024-01-01 18:47:44 +0000

    fixing checkout page

--- src/app/api/(checkout)/create-intent/[orderId]/route.tsx
+++ src/app/api/(checkout)/create-intent/[orderId]/route.tsx
@@ -1,42 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/utils/connect";
-import { NextRequest, NextResponse } from "next/server";
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { orderId: string } }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-  const { orderId } = params;
+  if (req.method === "POST") {
+    const { orderId } = req.query;
 
-  const order = await prisma.order.findUnique({
-    where: {
-      id: orderId,
-    },
-  });
-
-  if (order) {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 100 * 100,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
-
-    await prisma.order.update({
+    const order = await prisma.order.findUnique({
       where: {
-        id: orderId,
+        id: orderId as string,
       },
-      data: { intent_id: paymentIntent.id },
     });
 
-    return new NextResponse(
-      JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-      { status: 200 }
-    );
+    if (order) {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: 100 * 100,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+
+      await prisma.order.update({
+        where: {
+          id: orderId as string,
+        },
+        data: { intent_id: paymentIntent.id },
+      });
+
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } else {
+      res.status(404).json({ message: "Order not found!" });
+    }
   } else {
-    return new NextResponse(JSON.stringify({ message: "Order not found!" }), {
-      status: 404,
-    });
+    // Handle any other HTTP methods
+    res.status(405).end(); // Method Not Allowed
   }
 }

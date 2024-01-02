3a6f3a7dfd18a39c2a6a134b044af0a1417f7120
Noonsoo <nwafornonso70@gmail.com>
2024-01-02 16:42:00 +0000

    fixing intent

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use server";
 import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
---
+++ src/components/AddPageComponent.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import AddPageProps from "./AddPageProps";
+import { useRouter } from "next/navigation";
+
+function AddPageComponent({
+  file,
+  option,
+  inputs,
+  options,
+  handleChange,
+  handleChangeImg,
+  changeOption,
+  setOptions,
+}: any) {
+  const router = useRouter();
+
+  const upload = async () => {
+    const data = new FormData();
+    data.append("file", file!);
+    data.append("upload_preset", "restaurant");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/lamadev/image", {
+      method: "POST",
+      headers: { "Content-Type": "multipart/form-data" },
+      body: data,
+    });
+
+    const resData = await res.json();
+    return resData.url;
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    "use server";
+    e.preventDefault();
+
+    try {
+      const url = await upload();
+      const res = await fetch("http://localhost:3000/api/products", {
+        method: "POST",
+        body: JSON.stringify({
+          img: url,
+          ...inputs,
+          options,
+        }),
+      });
+
+      const data = await res.json();
+
+      router.push(`/product/${data.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return <AddPageProps handleSubmit={handleSubmit} />;
+}
+
+export default AddPageComponent;

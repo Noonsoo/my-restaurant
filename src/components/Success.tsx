2d017f2cd77e78d5b5c6fa30d713d1f53f788a2f
Noonsoo <nwafornonso70@gmail.com>
2024-01-02 15:42:12 +0000

    fixing intent

---
+++ src/components/Success.tsx
@@ -0,0 +1,26 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+
+import ConfettiExplosion from "react-confetti-explosion";
+
+function Success({ makeRequest }: any) {
+  const searchParams = useSearchParams();
+  const payment_intent = searchParams!.get("payment_intent");
+  const router = useRouter();
+  useEffect(() => {
+    makeRequest(payment_intent);
+
+    makeRequest();
+  }, [payment_intent, router]);
+  return (
+    <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
+      <p className="max-w-[600px]">
+        Payment successful. You are being redirected to the orders page. Please
+        do not close the page.
+      </p>
+      <ConfettiExplosion className="absolute m-auto" />
+    </div>
+  );
+}
+
+export default Success;
